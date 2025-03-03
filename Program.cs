ead4980868e4acbd98910afd8b5002c46fa10da1
Jakub Adamčík <59259Adamcik@student.sstebrno.cz>
2025-03-03 11:51:42 +0100

    setup

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("_Host");
 
 app.Run();
 
