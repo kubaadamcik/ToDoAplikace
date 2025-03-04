389cacd35c52787c882f4e86ef95aea0e215486e
Jakub Adamčík <59259Adamcik@student.sstebrno.cz>
2025-03-04 11:17:53 +0100

    database

--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -3,8 +3,10 @@ using ToDoAplikace.Models;
 
 namespace ToDoAplikace.Data
 {
-    public class DatabaseContext : DbContext
+    public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
     {
         public DbSet<ToDoTask> Tasks { get; set; }
     }
+
+
 }
--- Models/ToDoTask.cs
+++ Models/ToDoTask.cs
@@ -4,11 +4,17 @@ namespace ToDoAplikace.Models
 {
     public class ToDoTask
     {
+        public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public DateTime CreatedAt { get; set; }
         public bool Completed { get; set; }
 
-
+        public ToDoTask(string name, DateTime createdAt)
+        {
+            Name = name;
+            CreatedAt = createdAt;
+        }
     }
 
 }
