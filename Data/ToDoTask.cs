67d4adf251f718ac2c6173787ad2a22c4b9c5b4c
kubaadamcik <jakubadamcik11@gmail.com>
2025-03-19 09:36:18 +0100

    Made DateTime

--- Data/ToDoTask.cs
+++ Data/ToDoTask.cs
@@ -10,7 +10,7 @@ namespace ToDoAplikace.Data
         public string Name { get; set; }
 
         [Required]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         public DateTime CreatedAt { get; set; }
         public DateTime? Deadline { get; set; }
