35d6dac0b635d7bd68a231e8dafd2f951205851c
kubaadamcik <jakubadamcik11@gmail.com>
2025-03-27 08:33:02 +0100

    quick

--- Data/ChangeUsernameModel.cs
+++ Data/ChangeUsernameModel.cs
@@ -4,7 +4,7 @@ namespace ToDoAplikace.Data
 {
     public class ChangeUsernameModel
     {
-        [Required] public string UserName { get; set; }
-        [Required] public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserId { get; set; }
     }
 }
