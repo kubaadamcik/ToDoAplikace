c66ca3b8653c30b72609ae322988c511abff8e4b
kubaadamcik <jakubadamcik11@gmail.com>
2025-03-28 14:20:19 +0100

    Fixed changing username and created a success modal

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazorBootstrap();
 
 builder.Services.AddHttpClient();
 
